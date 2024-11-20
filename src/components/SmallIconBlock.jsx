fa86e1221190faab6698161b1aabad82b0a485fd
Alex <beachgirl.ak@gmail.com>
2024-11-20 15:55:20 -0600

    Small icon block

---
+++ src/components/SmallIconBlock.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import * as PIcons from "@phosphor-icons/react";
+
+function SmallIconBlock({
+  text,
+  iconName,
+  iconSide = "right",
+  style = "fill",
+}) {
+  var IconComponent = PIcons[iconName];
+
+  return (
+    <div className={`small-icon-block ${style == "fill" ? "fill" : "outline"}`}>
+      {iconSide == "right" ? (
+        <>
+          <h6>{text}</h6>
+          <IconComponent size={40} />
+        </>
+      ) : (
+        <>
+          <IconComponent size={40} />
+          <h6>{text}</h6>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default SmallIconBlock;
--- src/pages/overview.js
+++ src/pages/overview.js
@@ -5,6 +5,7 @@ import { designTable, webAppTable } from "./info/tableInfo";
 
 import badgeLogo from "../images/BadgeLogo.svg"
 import Tables from "../components/Tables";
+import SmallIconBlock from "../components/SmallIconBlock";
 
 
 
@@ -41,6 +42,27 @@ function Overview() {
                 <div className="horizontal-line"></div>
                 <div className="content">
                     <h2>High-level Project Overview</h2>
+                    <div className="multiple-sib">
+                        <SmallIconBlock 
+                            text={"Web App"}
+                            iconName={"Desktop"}
+                        />
+                        <SmallIconBlock 
+                            text={"Modular"}
+                            iconName={"SquaresFour"}
+                            iconSide="left"
+                        />
+                        <SmallIconBlock 
+                            text={"Profiles"}
+                            iconName={"UserCircle"}
+                        />
+                        <SmallIconBlock 
+                            text={"blount"}
+                            iconName={"GraduationCap"}
+                            iconSide="left"
+                        />
+                    </div>
+                    <p>The Blount Web App is designed to serve the Blount Scholars Program at the University of Alabama, providing a dynamic, interactive platform for Blount students, alumni, and community members. Its primary goal is to foster community engagement, offering a space for students to interact, connect with alumni, or simply take a study break in an environment tailored to both work and leisure. The web application needs to be modular in all aspects to allow for future additions.</p>
                     <Tables 
                         title={designTable.title}
                         content={designTable.content}
